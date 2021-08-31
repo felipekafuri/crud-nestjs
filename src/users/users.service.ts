6bb5f4fc4435865273fe39f5b5ecb6e45b8b13e7
felipekafuri <felipekafuri@discente.ufg.br>
2021-08-31 11:43:39 -0300

    users requests and endpoints

---
+++ src/users/users.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Injectable()
+export class UsersService {
+  create({ email, name, password }: CreateUserDto) {
+    const user = {
+      id: 1,
+      email,
+      name,
+      password,
+    };
+    return user;
+  }
+
+  findAll() {
+    return `This action returns all users`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
