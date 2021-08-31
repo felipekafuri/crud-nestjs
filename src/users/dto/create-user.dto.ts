e1b2b5262c540881c992172488ea6caa8df68af7
felipekafuri <felipekafuri@discente.ufg.br>
2021-08-31 12:37:23 -0300

    crud

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,6 @@
-export class CreateUserDto {
+import { User } from '../entities/user.entity';
+
+export class CreateUserDto extends User {
   name: string;
   email: string;
   password: string;
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,25 +1,46 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-  create({ email, name, password }: CreateUserDto) {
-    const user = {
+  private users: User[] = [
+    {
       id: 1,
+      name: 'John Doe',
+      email: 'john@example.com',
+      password: '123456',
+    },
+  ];
+
+  create({ email, name, password }: CreateUserDto) {
+    const id = this.users[this.users.length - 1].id + 1;
+
+    const user: User = {
+      id,
       email,
       name,
       password,
     };
+
+    const userExists = this.users.find((user) => user.email === email);
+
+    if (userExists) {
+      return 'User already exists';
+    }
+
+    this.users.push(user);
+
     return user;
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.users;
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} user`;
+    return this.users.find((user) => user.id === id);
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
@@ -27,6 +48,8 @@ export class UsersService {
   }
 
   remove(id: number) {
-    return `This action removes a #${id} user`;
+    const findIndex = this.users.findIndex((user) => user.id === id);
+    this.users.splice(findIndex, 1);
+    return;
   }
 }
